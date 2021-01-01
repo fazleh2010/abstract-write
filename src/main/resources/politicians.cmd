b3196448d98fac20fbc1fb10f4ab068a21524dd0
elahi <fazleh2010@gmail.com>
2021-01-02 00:30:28 +0100

    new input file added

--- src/main/resources/politicians.cmd
+++ src/main/resources/politicians.cmd
@@ -7,3 +7,17 @@ cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Flim>" | sed --exp
 
 cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Mountain>" | sed --expression 's/ <http:\/\/www.w3.*//' > Mountain.txt
 
+
+cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Organisation>" | sed --expression 's/ <http:\/\/www.w3.*//' > Organisation.txt
+
+cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/MilitaryConflict>" | sed --expression 's/ <http:\/\/www.w3.*//' > MilitaryConflict.txt
+
+
+cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Airport>" | sed --expression 's/ <http:\/\/www.w3.*//' > Airport.txt
+
+
+cat instance-types_en.nt | grep "<http://dbpedia.org/ontology/Species>" | sed --expression 's/ <http:\/\/www.w3.*//' > Species.txt
+
+
+MilitaryConflict
+
